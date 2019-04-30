f59c9329d0f5e25b73789a9609db2c775c94621a
FiddyOnFiddy <matthewbiggins@hotmail.co.uk>
2019-05-01 00:06:57 +0100

    sda

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    public int speed = 10;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+
+        if (Input.GetKey(KeyCode.W) && transform.tag == "Player1" && transform.position.y <= 4.25f)
+        {
+            Vector2 currentPos = transform.position;
+
+            currentPos.y += 1 * speed * Time.deltaTime;
+
+            transform.position = currentPos;
+        }
+        else if (Input.GetKey(KeyCode.S) && transform.tag == "Player1" && transform.position.y >= -4.25f)
+        {
+            Vector2 currentPos = transform.position;
+
+            currentPos.y -= 1 * speed * Time.deltaTime;
+
+            transform.position = currentPos;
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow) && transform.tag == "Player2" && transform.position.y <= 4.25f)
+        {
+            Vector2 currentPos = transform.position;
+
+            currentPos.y += 1 * speed * Time.deltaTime;
+
+            transform.position = currentPos;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow) && transform.tag == "Player2" && transform.position.y >= -4.25f)
+        {
+            Vector2 currentPos = transform.position;
+
+            currentPos.y -= 1 * speed * Time.deltaTime;
+
+            transform.position = currentPos;
+        }
+
+    }
+}
---
+++ Assets/TesticleMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TesticleMovement : MonoBehaviour
+{
+    public float randomRotation;
+    public int rotationDir;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotationDir = Random.Range(1, 4);
+        switch (rotationDir)
+        {
+        case 4:
+                randomRotation = Random.Range(45.0f, 75.0f);
+                break;
+        case 3:
+                randomRotation = Random.Range(105.0f, 135.0f);
+                break;
+        case 2:
+                randomRotation = Random.Range(225.0f, 255.0f);
+                break;
+        case 1:
+                randomRotation = Random.Range(285.0f, 315.0f);
+                break;
+        }
+
+        Debug.Log(rotationDir);
+
+        Vector3 currentRotation = transform.eulerAngles;
+        currentRotation.z = randomRotation;
+        transform.eulerAngles = currentRotation;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
